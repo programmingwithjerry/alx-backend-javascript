ecfdf17347eeebc70d89b3c5bb0f7f91046a2e40
programmingwithjerry <eneyellowj@gmail.com>
2024-11-02 14:01:47 +0100

    100-await.js

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,15 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default async function asyncUploadUser() {
+  let result = {};
+
+  try {
+    const photo = await uploadPhoto();
+    const user = await createUser();
+    result = { photo, user };
+  } catch (error) {
+    result = { photo: null, user: null };
+  }
+  
+  return result;
+}
