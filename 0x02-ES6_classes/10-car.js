69787f65a85ed3b6a69d9bb4992881ffe1d4201e
programmingwithjerry <eneyellowj@gmail.com>
2024-11-03 19:53:08 +0100

    10-car.js

---
+++ 0x02-ES6_classes/10-car.js
@@ -0,0 +1,30 @@
+export default class Car {
+  constructor(brand, motor, color) {
+    this._brand = brand; // Store brand
+    this._motor = motor; // Store motor
+    this._color = color; // Store color
+  }
+
+  // Method to create a new instance of the Car class
+  cloneCar() {
+    const Clone = this.constructor[Symbol.species] || this.constructor; // Check for a custom species or use the current constructor
+    return new Clone(this._brand, this._motor, this._color);
+  }
+
+  // Getter for brand
+  get brand() {
+    return this._brand;
+  }
+
+  // Getter for motor
+  get motor() {
+    return this._motor;
+  }
+
+  // Getter for color
+  get color() {
+    return this._color;
+  }
+}
+
+// To enable custom cloning logic, you can define the Symbol.species property in subclasses if needed
