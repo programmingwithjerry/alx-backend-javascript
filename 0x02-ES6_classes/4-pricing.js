b9423fbbc5678604781eb891ffa7cf4b37f1925f
programmingwithjerry <eneyellowj@gmail.com>
2024-11-03 17:03:35 +0100

    4-pricing.js

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,52 @@
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    if (typeof amount !== 'number') {
+      throw new TypeError('Amount must be a number');
+    }
+    if (!(currency instanceof Currency)) {
+      throw new TypeError('Currency must be an instance of the Currency class');
+    }
+
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  // Getter and setter for amount
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(newAmount) {
+    if (typeof newAmount !== 'number') {
+      throw new TypeError('Amount must be a number');
+    }
+    this._amount = newAmount;
+  }
+
+  // Getter and setter for currency
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(newCurrency) {
+    if (!(newCurrency instanceof Currency)) {
+      throw new TypeError('Currency must be an instance of the Currency class');
+    }
+    this._currency = newCurrency;
+  }
+
+  // Method to display full price
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
+  }
+
+  // Static method to convert price
+  static convertPrice(amount, conversionRate) {
+    if (typeof amount !== 'number' || typeof conversionRate !== 'number') {
+      throw new TypeError('Both amount and conversion rate must be numbers');
+    }
+    return amount * conversionRate;
+  }
+}
