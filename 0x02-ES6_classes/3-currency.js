b23192afb96f5f5d9ecd17d0e7b0e5f3946807a2
programmingwithjerry <eneyellowj@gmail.com>
2024-11-03 16:55:06 +0100

    3-currency.js

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,42 @@
+export default class Currency {
+  constructor(code, name) {
+    if (typeof code !== 'string') {
+      throw new TypeError('Code must be a string');
+    }
+    if (typeof name !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+
+    this._code = code;
+    this._name = name;
+  }
+
+  // Getter and setter for code
+  get code() {
+    return this._code;
+  }
+
+  set code(newCode) {
+    if (typeof newCode !== 'string') {
+      throw new TypeError('Code must be a string');
+    }
+    this._code = newCode;
+  }
+
+  // Getter and setter for name
+  get name() {
+    return this._name;
+  }
+
+  set name(newName) {
+    if (typeof newName !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = newName;
+  }
+
+  // Method to display the full currency format
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
+}
