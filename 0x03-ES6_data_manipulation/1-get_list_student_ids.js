6a93508590c62183a50efc9d6ce40c71987d99f6
programmingwithjerry <eneyellowj@gmail.com>
2024-11-04 12:26:35 +0100

    1-get_list_student_ids.js

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,6 +1,4 @@
 export default function getListStudentIds(obj) {
-  if (!Array.isArray(obj)) {
-    return [];
-  }
-  return obj.map(item => item.id);
+  if (!(obj instanceof Array)) return [];
+  return obj.map((item) => item.id);
 }
