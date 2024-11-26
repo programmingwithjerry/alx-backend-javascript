ba8c458fed5f9bbf565d54d7e28d8e2dd9834412
progarammingwithjerry <eneyellowj@gmail.com>
2024-11-26 17:10:03 +0100

    0-console.js

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+/**
+ * This script defines a function `displayMessage` that prints a given string
+ * argument to standard output (STDOUT).
+ */
+
+function displayMessage(message) {
+    console.log(message);
+}
+
+// Example usage
+   displayMessage('Hello, World!');
