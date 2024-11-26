a0e09f4e96c46d145576f884213ff0161a0fd0a9
progarammingwithjerry <eneyellowj@gmail.com>
2024-11-26 17:19:41 +0100

    0-console.js

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,12 +1,16 @@
 #!/usr/bin/env node
 /**
  * This script defines a function `displayMessage` that prints a given string
- * argument to standard output (STDOUT).
+ * to standard output (STDOUT) and exports the function for external use.
  */
 
+/**
+ * Prints the given message to the standard output.
+ * @param {string} message - The message to print.
+ */
 function displayMessage(message) {
-    console.log(message);
+  console.log(message);
 }
 
-// Example usage
-   displayMessage('Hello, World!');
+// Export the function to make it available for external use
+module.exports = displayMessage;
